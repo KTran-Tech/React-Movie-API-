9e626bef4b589e6cdebda77f765ca3e365bef824
KTran-Tech <kevinmt191919@gmail.com>
2020-05-14 21:42:50 -0500

    Search Functionality and CSS

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+import Search from './components/Search';
+import Results from './components/Results';
+
+
+function App() {
+
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  })
+
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=554321c7";
+
+  const search = (e) => {
+    if(e.key === 'Enter'){
+      axios(apiurl+'&s='+state.s)
+          .then(({data})=>{
+            console.log({data});
+            let results = data.Search;
+
+            setState(prevState =>{
+              return {...prevState, results: results}
+            })
+          })
+    }
+  }
+
+
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+    
+    setState(prevState => {
+      return{...prevState, s: s}
+    })
+
+    console.log(state.s);
+  }
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Movie Database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results} />
+      </main>
+    </div>
+  );
+
+}
+
+export default App;
